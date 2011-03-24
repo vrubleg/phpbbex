343566ba94a04ed4249d03604c0309047d10b427
Evgeny Vrublevsky <me@veg.by>
2011-03-25 00:08:15 +0200

    Thumbnails on demand

--- download/file.php
+++ download/file.php
@@ -417,6 +417,16 @@ function send_file_to_browser($attachment, $upload_dir, $category)
 
 	$filename = $phpbb_root_path . $upload_dir . '/' . $attachment['physical_filename'];
 
+	if (!@file_exists($filename) && substr($attachment[physical_filename],0,6) == 'thumb_')
+	{
+		$image_file = substr($attachment[physical_filename],6);
+		include_once("../includes/functions_posting.php");
+		if (! create_thumbnail($phpbb_root_path . $upload_dir . '/' . $image_file, $filename, '')) 
+		{ // disable thumbnail 
+			$db->sql_query('UPDATE ' . ATTACHMENTS_TABLE . ' SET thumbnail = 0 WHERE attach_id = ' . $attachment['attach_id']);
+		};
+	};
+
 	if (!@file_exists($filename))
 	{
 		trigger_error($user->lang['ERROR_NO_ATTACHMENT'] . '<br /><br />' . sprintf($user->lang['FILE_NOT_FOUND_404'], $filename));
