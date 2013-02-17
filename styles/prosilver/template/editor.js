67f68089894ee16ece4bd2ab6ed16649d9a1f573
Evgeny Vrublevsky <me@veg.by>
2013-02-17 19:48:57 +0300

    Auto bullet [*] inserter for the [list] BBCode

--- styles/prosilver/template/editor.js
+++ styles/prosilver/template/editor.js
@@ -51,16 +51,60 @@ function initInsertions()
 * bbstyle
 */
 function bbstyle(bbnumber)
-{	
+{
+	var textarea = document.forms[form_name].elements[text_name];
 	if (bbnumber != -1)
 	{
 		bbfontstyle(bbtags[bbnumber], bbtags[bbnumber+1]);
 	} 
-	else 
+	else if (!textarea.selectionEnd || (textarea.selectionEnd - textarea.selectionStart == 0))
 	{
 		insert_text('[*]');
 	}
-	document.forms[form_name].elements[text_name].focus();
+	else
+	{
+		// Automatic [*] for each line
+		var selLength = (typeof(textarea.textLength) == 'undefined') ? textarea.value.length : textarea.textLength;
+		var selStart = textarea.selectionStart;
+		var selEnd = textarea.selectionEnd;
+		var scrollTop = textarea.scrollTop;
+
+		if (selEnd == 1 || selEnd == 2) 
+		{
+			selEnd = selLength;
+		}
+
+		var before = (textarea.value).substring(0,selStart);
+		var selected = (textarea.value).substring(selStart, selEnd);
+		var after = (textarea.value).substring(selEnd, selLength);
+
+		var parts = selected.match(/^(\s*\[list[\w\d=]*\])((?:.|[\r\n])*)(\[\/list\]\s*)$/i);
+		if (parts)
+		{
+			before += parts[1];
+			selected = parts[2];
+			after = parts[3] + after;
+		}
+
+		var items = selected.split(/\r\n|\r|\n/);
+		selected = '';
+		var is_first = true;
+		jQuery.each(items, function(index, value)
+		{
+			if (!is_first) selected += '\n';
+			is_first = false;
+			value = jQuery.trim(value);
+			if (!value) return true;
+			if (value.indexOf('[*]') !== 0) selected += '[*]';
+			selected += value;
+		});
+
+		textarea.value = before + selected + after;
+		textarea.selectionStart = before.length;
+		textarea.selectionEnd = before.length + selected.length;
+		textarea.scrollTop = scrollTop;
+	}
+	textarea.focus();
 }
 
 /**
