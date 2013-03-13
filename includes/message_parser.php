6b212c1a10aea61b183e1ffae919ebe0cda203e3
Evgeny Vrublevsky <me@veg.by>
2013-03-13 15:36:24 +0300

    The [tt] bbcode has more priority than the [code] bbcode

--- includes/message_parser.php
+++ includes/message_parser.php
@@ -112,8 +112,8 @@ class bbcode_firstpass extends bbcode
 		// To parse multiline URL we enable dotall option setting only for URL text
 		// but not for link itself, thus [url][/url] is not affected.
 		$this->bbcodes = array(
-			'code'			=> array('bbcode_id' => 8,	'regexp' => array('#\[code(?:=([a-z]+))?\](.+\[/code\])#uise' => "\$this->bbcode_code('\$1', '\$2')")),
 			'tt'			=> array('bbcode_id' => 14,	'regexp' => array('#\[tt\](.*?)\[/tt\]#uise' => "\$this->bbcode_teletype('\$1')")),
+			'code'			=> array('bbcode_id' => 8,	'regexp' => array('#\[code(?:=([a-z]+))?\](.+\[/code\])#uise' => "\$this->bbcode_code('\$1', '\$2')")),
 			'upd'			=> array('bbcode_id' => 15,	'regexp' => array('#\[upd=(\d+(?:[:]\d+){0,3})\](.*?)\[/upd\]#uise' => "\$this->bbcode_upd('\$1', '\$2')")),
 			'quote'			=> array('bbcode_id' => 0,	'regexp' => array('#\[quote(?:=&quot;(.*?)&quot;)?\](.+)\[/quote\]#uise' => "\$this->bbcode_quote('\$0', 'quote')")),
 			'spoiler'		=> array('bbcode_id' => 16,	'regexp' => array('#\[spoiler(?:=&quot;(.*?)&quot;)?\](.+)\[/spoiler\]#uise' => "\$this->bbcode_quote('\$0', 'spoiler')")),
