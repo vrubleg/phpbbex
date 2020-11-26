6ebb5767c0a52a1e01868860232ba9d502908225
Evgeny Vrublevsky <me@veg.by>
2020-11-26 19:41:05 +0300

    Don't add X-Priority headers to emails because it may make SpamAssassin unhappy.

--- includes/functions_messenger.php
+++ includes/functions_messenger.php
@@ -22,8 +22,9 @@ if (!defined('IN_PHPBB'))
 */
 class messenger
 {
-	var $vars, $msg, $extra_headers, $replyto, $from, $subject;
+	var $vars, $msg, $replyto, $from, $subject;
 	var $addresses = array();
+	var $extra_headers = array();
 
 	var $mail_priority = MAIL_NORMAL_PRIORITY;
 	var $use_queue = true;
@@ -172,9 +173,9 @@ class messenger
 	*/
 	function anti_abuse_headers($config, $user)
 	{
-		$this->headers('X-AntiAbuse: Board servername - ' . mail_encode($config['server_name']));
-		$this->headers('X-AntiAbuse: User_id - ' . $user->data['user_id']);
-		$this->headers('X-AntiAbuse: Username - ' . mail_encode($user->data['username']));
+		$this->headers('X-AntiAbuse: Host Name - ' . mail_encode($config['server_name']));
+		$this->headers('X-AntiAbuse: User ID - ' . $user->data['user_id']);
+		$this->headers('X-AntiAbuse: User Name - ' . mail_encode($user->data['username']));
 		$this->headers('X-AntiAbuse: User IP - ' . $user->ip);
 	}
 
@@ -237,7 +238,7 @@ class messenger
 
 		$this->tpl_obj = &$this->tpl_msg[$template_lang . $template_file];
 		$this->vars = &$this->tpl_obj->_rootref;
-		$this->tpl_msg = '';
+		$this->tpl_msg = array();
 
 		return true;
 	}
@@ -442,13 +443,14 @@ class messenger
 		$headers[] = 'Content-Type: text/plain; charset=UTF-8'; // format=flowed
 		$headers[] = 'Content-Transfer-Encoding: 8bit'; // 7bit
 
-		$headers[] = 'X-Priority: ' . $this->mail_priority;
-		$headers[] = 'X-MSMail-Priority: ' . (($this->mail_priority == MAIL_LOW_PRIORITY) ? 'Low' : (($this->mail_priority == MAIL_NORMAL_PRIORITY) ? 'Normal' : 'High'));
-		$headers[] = 'X-Mailer: phpBB3';
-		$headers[] = 'X-MimeOLE: phpBB3';
-		$headers[] = 'X-phpBB-Origin: phpbb://' . str_replace(array('http://', 'https://'), array('', ''), generate_board_url());
+		// SpamAssassin doesn't like these headers =(
+		// $headers[] = 'X-Priority: ' . $this->mail_priority;
+		// $headers[] = 'X-MSMail-Priority: ' . (($this->mail_priority == MAIL_LOW_PRIORITY) ? 'Low' : (($this->mail_priority == MAIL_NORMAL_PRIORITY) ? 'Normal' : 'High'));
+		// $headers[] = 'X-Mailer: phpBB3';
+		// $headers[] = 'X-MimeOLE: phpBB3';
+		// $headers[] = 'X-phpBB-Origin: phpbb://' . str_replace(array('http://', 'https://'), array('', ''), generate_board_url());
 
-		if (sizeof($this->extra_headers))
+		if (count($this->extra_headers))
 		{
 			$headers = array_merge($headers, $this->extra_headers);
 		}
