ce84d671c8880acd8220341c45871277cd4f5bb8
Evgeny Vrublevsky <me@veg.by>
2021-05-27 23:59:53 +0300

    Fix a potential PHP warning.

--- includes/ucp/ucp_profile.php
+++ includes/ucp/ucp_profile.php
@@ -559,8 +559,8 @@ class ucp_profile
 					'IMG_STATUS'			=> ($config['allow_sig_img']) ? $user->lang['IMAGES_ARE_ON'] : $user->lang['IMAGES_ARE_OFF'],
 					'FLASH_STATUS'			=> ($config['allow_sig_flash']) ? $user->lang['FLASH_IS_ON'] : $user->lang['FLASH_IS_OFF'],
 					'URL_STATUS'			=> ($config['allow_sig_links']) ? $user->lang['URL_IS_ON'] : $user->lang['URL_IS_OFF'],
-					'MIN_FONT_SIZE'			=> (int) $config['min_sig_font_size'],
-					'MAX_FONT_SIZE'			=> (int) $config['max_sig_font_size'],
+					'MIN_FONT_SIZE'			=> isset($config['min_sig_font_size']) ? (int) $config['min_sig_font_size'] : 100,
+					'MAX_FONT_SIZE'			=> isset($config['max_sig_font_size']) ? (int) $config['max_sig_font_size'] : 100,
 
 					'L_SIGNATURE_EXPLAIN'	=> sprintf($user->lang['SIGNATURE_EXPLAIN'], $config['max_sig_chars'], $config['max_sig_lines'] ? $config['max_sig_lines'] : $user->lang['NO']),
 
