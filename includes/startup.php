ed1273a75bdbb87de1e7f4a2c75b82dea0f951be
Evgeny Vrublevsky <me@veg.by>
2025-06-28 15:29:20 +0300

    PCRE is always with UTF-8 support these days.

--- includes/startup.php
+++ includes/startup.php
@@ -12,7 +12,6 @@ if (!defined('IN_PHPBB'))
 
 // Check PHP version.
 if (version_compare(PHP_VERSION, '7.2', '<')) { die('PHP 7.2+ is required.'); }
-if (@preg_match('/\p{L}/u', 'a') === false) { die('PCRE with UTF-8 support is required.'); }
 if (!extension_loaded('mbstring')) { die('mbstring is required.'); }
 
 if (file_exists(PHPBB_ROOT_PATH . 'config.php'))
