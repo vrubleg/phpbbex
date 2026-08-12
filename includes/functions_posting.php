c473b2bbe525dba4574e037dd4d64683fe5f9bbe
Evgeny Vrublevsky <me@veg.by>
2026-08-12 13:38:56 +0300

    Don't spam moderator logs with post edits when reparsing BBCode

--- includes/functions_posting.php
+++ includes/functions_posting.php
@@ -1781,7 +1781,7 @@ function submit_post($mode, $subject, $username, $topic_type, &$poll, &$data, $u
 
 			// If the person editing this post is different to the one having posted then we will add a log entry stating the edit
 			// Could be simplified by only adding to the log if the edit is not tracked - but this may confuse admins/mods
-			if ($user->data['user_id'] != $poster_id)
+			if ($mode != 'reparse' && $user->data['user_id'] != $poster_id)
 			{
 				$log_subject = $subject ?: $data['topic_title'];
 				add_log('mod', $data['forum_id'], $data['topic_id'], 'LOG_POST_EDITED', $log_subject, (!empty($username)) ? $username : $user->lang['GUEST']);
