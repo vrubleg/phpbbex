8c57c31e1b5dd7df6c6c41f39b723a6cb67f4157
Evgeny Vrublevsky <me@veg.by>
2026-09-02 21:17:44 +0300

    Gallery: Remove obsolete ACP maintenance actions

--- includes/acp/acp_gallery.php
+++ includes/acp/acp_gallery.php
@@ -66,75 +66,24 @@ class acp_gallery
 		if (!confirm_box(true))
 		{
 			$confirm = false;
-			$album_id = 0;
 			switch ($action)
 			{
 				case 'resync':
 					$confirm = true;
 					$confirm_lang = 'RESYNC_GALLERY_STATS_CONFIRM';
 				break;
-				case 'reset_rating':
-					$album_id = request_var('reset_album_id', 0);
-					$album_data = phpbb_gallery_album::get_info($album_id);
-					$confirm = true;
-					$confirm_lang = sprintf($user->lang['RESET_RATING_CONFIRM'], $album_data['album_name']);
-				break;
 				case 'purge_cache':
 					$confirm = true;
 					$confirm_lang = 'PURGE_THUMBNAIL_CACHE_EXPLAIN';
-				break;
-				case 'create_pega':
-					$confirm = false;
-					if (!$auth->acl_get('a_board'))
-					{
-						trigger_error($user->lang['NO_AUTH_OPERATION'] . adm_back_link($this->u_action), E_USER_WARNING);
-					}
-
-					$username = request_var('username', '', true);
-					$user_id = 0;
-					if ($username)
-					{
-						if (!function_exists('user_get_id_name'))
-						{
-							phpbb_gallery_url::_include('functions_user', 'phpbb');
-						}
-						user_get_id_name($user_id, $username);
-					}
-					if (is_array($user_id))
-					{
-						$user_id = $user_id[0] ?? 0;
-					}
-
-					$sql = 'SELECT username, user_colour, user_id
-						FROM ' . USERS_TABLE . '
-						WHERE user_id = ' . $user_id;
-					$result = $db->sql_query($sql);
-					$user_row = $db->sql_fetchrow($result);
-					$db->sql_freeresult($result);
-					if (!$user_row)
-					{
-						trigger_error($user->lang['NO_USER'] . adm_back_link($this->u_action), E_USER_WARNING);
-					}
-
-					$image_user = new phpbb_gallery_user($db, $user_row['user_id']);
-					$album_id = $image_user->get_data('personal_album_id');
-					if ($album_id)
-					{
-						trigger_error($user->lang('PEGA_ALREADY_EXISTS', $user_row['username']) . adm_back_link($this->u_action), E_USER_WARNING);
-					}
-					phpbb_gallery_album::generate_personal_album($user_row['username'], $user_row['user_id'], $user_row['user_colour'], $image_user);
-
-					trigger_error($user->lang('PEGA_CREATED', $user_row['username']) . adm_back_link($this->u_action));
 				break;
 			}
 
 			if ($confirm)
 			{
-				confirm_box(false, (($album_id) ? $confirm_lang : $user->lang[$confirm_lang]), build_hidden_fields([
+				confirm_box(false, $user->lang[$confirm_lang], build_hidden_fields([
 					'i'         => $id,
 					'mode'      => $mode,
 					'action'    => $action,
-					'reset_album_id'    => $album_id,
 				]));
 			}
 		}
@@ -259,28 +208,6 @@ class acp_gallery
 					trigger_error($user->lang['RESYNCED_GALLERY_STATS'] . adm_back_link($this->u_action));
 				break;
 
-				case 'reset_rating':
-					$album_id = request_var('reset_album_id', 0);
-
-					$image_ids = [];
-					$sql = 'SELECT image_id
-						FROM ' . GALLERY_IMAGES_TABLE . '
-						WHERE image_album_id = ' . $album_id;
-					$result = $db->sql_query($sql);
-					while ($row = $db->sql_fetchrow($result))
-					{
-						$image_ids[] = $row['image_id'];
-					}
-					$db->sql_freeresult($result);
-
-					if (!empty($image_ids))
-					{
-						phpbb_gallery_image_rating::delete_ratings($image_ids, true);
-					}
-
-					trigger_error($user->lang['RESET_RATING_COMPLETED'] . adm_back_link($this->u_action));
-				break;
-
 				case 'purge_cache':
 					if ($user->data['user_type'] != USER_FOUNDER)
 					{
@@ -349,8 +276,6 @@ class acp_gallery
 			'MEDIUM_DIR_SIZE'       => get_formatted_filesize(!empty($dir_sizes['stat_medium']) ? $dir_sizes['stat_medium'] : 0),
 			'CACHE_DIR_SIZE'        => get_formatted_filesize(!empty($dir_sizes['stat_cache']) ? $dir_sizes['stat_cache'] : 0),
 			'GALLERY_VERSION'       => phpbb_gallery_config::get('version'),
-			'U_FIND_USERNAME'       => phpbb_gallery_url::append_sid('phpbb', 'memberlist', 'mode=searchuser&amp;form=action_create_pega_form&amp;field=username&amp;select_single=true'),
-			'S_SELECT_ALBUM'        => phpbb_gallery_album::get_albumbox(false, 'reset_album_id', false, false, false, phpbb_gallery_album::PUBLIC_ALBUM, phpbb_gallery_album::TYPE_UPLOAD),
 
 			'S_FOUNDER'             => ($user->data['user_type'] == USER_FOUNDER),
 			'U_ACTION'              => $this->u_action,
