b6df3f31449ded21ebba0e665b394bd47a121db0
Evgeny Vrublevsky <me@veg.by>
2026-09-03 16:58:55 +0300

    Align private message mini-profile fields

--- includes/ucp/ucp_pm_viewmessage.php
+++ includes/ucp/ucp_pm_viewmessage.php
@@ -181,7 +181,7 @@ function view_message($id, $mode, $folder_id, $msg_id, $folder, $message_row)
 		'RANK_TITLE'        => $user_info['rank_title'],
 		'RANK_IMG'          => $user_info['rank_image'],
 		'AUTHOR_AVATAR'     => $user_info['avatar'] ?? '',
-		'AUTHOR_JOINED'     => $user->format_date($user_info['user_regdate']),
+		'AUTHOR_JOINED'     => $user->format_date($user_info['user_regdate'], false, false, true),
 		'AUTHOR_WITH_US'    => !empty($config['style_mp_show_with_us']) ? get_verbal_time_delta($user_info['user_regdate'], time(), false, 2) : '',
 		'AUTHOR_POSTS'      => (int) $user_info['user_posts'],
 		'AUTHOR_TOPICS'     => (int) $user_info['user_topics'],
